39c2652345ff0d3682ac46de5e7267f2d63a4478
Vera <vomichael>
2020-07-21 16:07:57 +0100

    made changes

--- gatsby-site/gatsby-config.js
+++ gatsby-site/gatsby-config.js
@@ -27,14 +27,6 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    {
-        resolve: `gatsby-source-filesystem`,
-        options: {
-          path: `${__dirname}/src/markdown-touch`,
-          name: `markdown-pages`,
-        },
-      },
-      `gatsby-transformer-remark`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
